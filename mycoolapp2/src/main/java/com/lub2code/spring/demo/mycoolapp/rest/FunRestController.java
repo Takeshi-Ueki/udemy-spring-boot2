83ae542436211e0db46ce8951f1efbfbe664c8e0
Takeshi-Ueki <takeshi.c.0104@gmail.com>
2023-09-08 20:18:42 +0900

    first commit.

---
+++ mycoolapp2/src/main/java/com/lub2code/spring/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,35 @@
+package com.lub2code.spring.demo.mycoolapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+	// propertyファイルから文字列を取得
+	@Value("${coach.name}")
+	private String coachName;
+	@Value("${team.name}")
+	private String teamName;
+	
+	@GetMapping("/teaminfo")
+	public String getTeamInfo() {
+		return "コーチ：" + coachName + ",　チーム:" + teamName;
+	}
+	
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello World!";
+	}
+	
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		return "Run a hard 5k!";
+	}
+	
+	@GetMapping("/fortune")
+	public String getDailyFortune() {
+		return "Today is your lucky day.";
+	}
+}
